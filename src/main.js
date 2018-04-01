e1862cf0fe34cf78e46b93a3efd9b538e4fa24e0
Milan Bačkonja <milan.backonja@hotmail.com>
2018-04-01 15:43:24 +0200

    Initial commit - Vue, Webpack and Cordova

---
+++ src/main.js
@@ -0,0 +1,27 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue';
+import App from './App';
+import router from './router';
+import VueCordova from 'vue-cordova';
+
+Vue.config.productionTip = false;
+
+Vue.use(VueCordova);
+
+// add cordova.js only if serving the app through file://
+if (window.location.protocol === 'file:' || window.location.port === '3000') {
+  alert('HEHE');
+  const cordovaScript = document.createElement('script');
+  cordovaScript.setAttribute('type', 'text/javascript');
+  cordovaScript.setAttribute('src', 'cordova.js');
+  document.body.appendChild(cordovaScript);
+}
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
+});
